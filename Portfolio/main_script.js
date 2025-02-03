83c73f760f242f724d7ba667eaa0c3a19512430f
YanJi13 <jeanebernardbermudez@gmail.com>
2025-02-03 16:15:14 +0800

    Linking cards to respective URLs

--- Portfolio/main_script.js
+++ Portfolio/main_script.js
@@ -1,3 +1,24 @@
 document.getElementById('project-01').addEventListener('click', function () {
-    window.location.href = 'https://www.geeksforgeeks.org/';
+    window.location.href = 'https://github.com/YanJi13/Pythagorean-Calculator';
+});
+document.getElementById('project-02').addEventListener('click', function () {
+    window.location.href = 'https://github.com/YanJi13/YanJi-s-Trial';
+});
+document.getElementById('project-03').addEventListener('click', function () {
+    window.location.href = 'https://github.com/YanJi13/Just-A-Click';
+});
+document.getElementById('project-04').addEventListener('click', function () {
+    window.location.href = 'https://github.com/YanJi13/StringTokenizer_GTS';
+});
+document.getElementById('project-05').addEventListener('click', function () {
+    window.location.href = 'https://github.com/YanJi13/ChronoLog-Repo';
+});
+document.getElementById('project-06').addEventListener('click', function () {
+    window.location.href = 'https://github.com/YanJi13/TraceQuest-v1';
+});
+document.getElementById('project-07').addEventListener('click', function () {
+    window.location.href = 'https://yanji13.github.io/BermudezExplorePHTours.html';
+});
+document.getElementById('project-08').addEventListener('click', function () {
+    window.location.href = 'https://yanji13.github.io/BermudezCourseSchedule.html';
 });
